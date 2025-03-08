fb91276a5a46c0a939b14437a59ec4a95ef926d3
Jibran Khalil <dev.jibrankhalil@gmail.com>
2025-03-08 16:15:50 +0500

    jibrankhalil 8 march 2025 04:15 am feature branch

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('This is Versioning Practically')
+print("This is Versioning Practically")
+
+print("Now Git will become my skill set but I won't do"
+" chichorapan on LinkedIn € but add it as skill ")
